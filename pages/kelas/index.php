c86e9cdc21b88ef0232b56ccc316eab529413762
Muhammad Syahrul Ramdhan <msyahrulr12@gmail.com>
2022-09-17 23:30:10 +0700

    Add example kelas

--- pages/kelas/index.php
+++ pages/kelas/index.php
@@ -1,4 +1,10 @@
 <?php
 
-include '../../config/config.php';
+// include '../../config/config.php';
+include '../../model/kelas.php';
 
+$dataKelas = getDataKelas();
+
+while ($data = $dataKelas->fetch_assoc()) {
+    echo $data['id'];
+}
