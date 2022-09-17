aad1b0ce80a5dc2934bb9efecdac2b7402e26bec
Muhammad Syahrul Ramdhan <msyahrulr12@gmail.com>
2022-09-18 01:49:04 +0700

    Define table_name and add new function create data

--- model/kelas.php
+++ model/kelas.php
@@ -1,11 +1,19 @@
 <?php
 
 include dirname(__DIR__).'/config/config.php';
+include dirname(__DIR__).'/config/functions.php';
+
+define('TABLE_NAME', 'kelas');
+
+$tableColumns = [
+    'kode',
+    'kelas'
+];
 
 function getDataKelas() {
     global $conn;
 
-    $query = "SELECT * FROM kelas";
+    $query = sprintf("SELECT * FROM %s", TABLE_NAME);
     $stmt = $conn->query($query);
     
     return $stmt;
@@ -14,8 +22,31 @@ function getDataKelas() {
 function getDataKelasById($id) {
     global $conn;
 
-    $query = sprintf("SELECT * FROM kelas WHERE id = %d", $id);
+    $query = sprintf("SELECT * FROM %s WHERE id = %d", TABLE_NAME, $id);
     $stmt = $conn->query($query);
     
     return $stmt->fetch_assoc();
+}
+
+function createDataKelas($datas) {
+    global $conn;
+    global $tableColumns;
+
+    $columnQuery = '';
+    foreach ($tableColumns as $column) {
+        $columnQuery .= $column . ', ';
+    }
+    $columnQuery = substr($columnQuery, 0, -2);
+
+    $valueQuery = '';
+    foreach ($datas as $value) {
+        $valueQuery .= '\''.$value . '\', ';
+    }
+    $valueQuery = substr($valueQuery, 0, -2);
+
+    $query = sprintf("INSERT INTO %s (%s)
+    VALUES (%s)", TABLE_NAME, $columnQuery, $valueQuery);
+    $stmt = $conn->query($query);
+    
+    return $stmt;
 }
